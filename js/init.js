1b81eac7221bbde97b9b2941b27f67be910b514c
csk917 <33192261+csk917@users.noreply.github.com>
2020-02-24 13:08:24 +0900

    02-24 chg

--- js/init.js
+++ js/init.js
@@ -1,23 +1,4 @@
 
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.9.1/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.9.1/firebase-analytics.js"></script>
-
- 
-<script src="https://www.gstatic.com/firebasejs/7.9.1/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.9.1/firebase-database.js"></script>
-
-<script src="https://www.gstatic.com/firebasejs/7.9.1/firebase-firestore.js"></script>
-
-<script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
-
-<script>
 
   // Your web app's Firebase configuration
   var firebaseConfig = {
@@ -86,13 +67,4 @@ db.collection("/1").get().then((querySnapshot) => {
 
   var docRef = db.collection("/1").doc('Testa');
   
-docRef.get().then(function(doc) {
-    if (doc.exists) {
-        console.log("Document data:", doc.data());
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-}).catch(function(error) {
-    console.log("Error getting document:", error);
-});
+
